140f3159cb405fc6423ae025715f51b3ee964653
Guilherme Koch <guilherme.koch@improvebytech.com>
2025-05-27 14:21:06 +0100

    endpoint fix

--- page_tracker.js
+++ page_tracker.js
@@ -27,7 +27,7 @@
       ...data
     };
     
-    fetch("http://127.0.0.1:8000/trackPageView", {
+    fetch("http://127.0.0.1:8000/track", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(payload)
